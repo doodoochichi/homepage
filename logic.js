cdb802c254a9829d38f0de57ac6906b0990a6616
zea <93328643+zea17@users.noreply.github.com>
2022-08-08 18:10:11 +0430

    added drawrailroad to the draw function

--- logic.js
+++ logic.js
@@ -22,6 +22,7 @@ function windowResized() {
 function draw() {
     drawBackground()
     drawLocomotive()
+    drawRailroad()
 }
 
 // ─── background ─────────────────────────────────────────────────────────────────
